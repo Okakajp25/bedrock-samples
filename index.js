bf7c72f23d84955d79df79d8204f9142ad7ad2a5
おかーかじぇーぴー <kaitosatou0025@gmail.com>
2023-10-01 18:05:37 +0900

    aaa

---
+++ index.js
@@ -0,0 +1,13 @@
+export class playerdeta {
+  constructor(name, level) {
+    this.name = name;
+    this.level = level;
+  }
+}
+
+//democode
+
+const playdeta = new playerdeta(Okakajp, 10)
+
+console.log(playdeta.name)
+console.log(playdeta.level)
